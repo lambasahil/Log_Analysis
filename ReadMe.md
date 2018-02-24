7f238b2aa824fe1281d70b19122502ca406ede9a
Sahil Lamba <sahillamba@Sahils-MacBook-Pro.local>
2018-02-25 02:02:52 +0530

    Log Analysis

---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+# LOG ANALYSIS
+
+This project is part of Full Stack Nanodegree
+
+Aim of this project is to print report based on data in database by using python(2.7.10)
+and postgresql
+
+### How To Install<br>
+1.Install [Vagrant](https://www.vagrantup.com/) and [VirtualBox](https://www.virtualbox.org/)<br>
+2.Download or clone from github [fullstack-nandegree-vm repository](https://github.com/udacity/fullstack-nanodegree-vm)</br>
+3.Now we got newsdata.sql in our vagrant directory and now we are good to go.</br>
+
+### How to run<br>
+1. change directory to vagrant directory then<br>
+2. **vagrant up** command to run the vagrant on vm<br>
+3. **vagrant ssh** to login into vm<br>
+4. change directory to vagrant<br>
+5. use command **psql -d news -f newsdata.sql** to load database<br>
+    -use **\c** to connect to database="news"<br>
+    -use **\dt** to see the tables in database<br>
+    -use **\dv** to see the views in database<br>
+    -use **\q** to quit the database<br>
+6. use command **python log.py** to run the programm<br>
---
+++ log.py
@@ -0,0 +1,60 @@
+import psycopg2
+pg=psycopg2.connect("dbname=news")
+cursor=pg.cursor()
+
+class LogAnalysis:
+    def __init__(self):
+        check_query = "SELECT EXISTS (SELECT 1 from information_schema.tables where table_schema = 'public' and table_name ='v1');"
+        cursor.execute(check_query)
+        valid=cursor.fetchall()[0][0]
+        if not valid:
+            view1="create view v1 as select path,count(*) as num from log group by path order by num desc;"
+            cursor.execute(view1)
+            view2="create view v2 as select articles.name,v1.num from articles,v1 where v1.path like concat('%',articles.slug) limit 3;"
+            cursor.execute(view2)
+            view3="create view v3 as select articles.author,articles.slug,v2.num from v2,articles where articles.title=v2.title;"
+            cursor.execute(view3)
+            view4="create view v4 as select author,sum(num) as view from v3 group by author order by view desc;"
+            cursor.execute(view4)
+            view5="create view v5 as select authors.name,v4.view from v4,authors where authors.id=v4.author;"
+            cursor.execute(view5)
+            view6="create view v6 as select date(time),count(status) as error from log where status='404 NOT FOUND' group by date order by date;"
+            view7="create view v7 as select date(time),count(status) as status from log group by date order by date;"
+            view8="create view v8 as select v7.date,round((100.0*v6.error)/v7.status,2) as percentage from v6,v7 where v6.date=v7.date;"
+
+
+# for query 1 :-
+cursor.execute("select * from v2 limit 3;")
+
+result = cursor.fetchall()
+
+print("THE LIST OF POPULAR ARTICLES ARE:")
+
+for i in range(len(result)):
+    print result[i][0],',',result[i][1],'views'
+    # print("%s%d" % (result[i][0],result[i][1]))
+
+# for query 2 :-
+cursor.execute("select * from v5;")
+
+result = cursor.fetchall()
+
+print ("\n")
+print("THE LIST OF POPULAR AUTHORS ARE:")
+
+for i in range(len(result)):
+    print result[i][0],',',result[i][1],'views'
+
+# for query 3 :-
+cursor.execute("select to_char(date,'Mon DD,YYYY') as date,percentage from v8 where percentage>1.0;")
+
+result = cursor.fetchall()
+print ("\n")
+print("PERC ERROR MORE THAN 1.0:")
+
+for i in range(len(result)):
+    print result[i][0],',',result[i][1],'%'
+
+
+
+pg.close()
